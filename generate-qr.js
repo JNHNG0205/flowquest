b623a7cbb279472305513fc2591f16d414239675
JNHNG0205 <heng.teh0205@gmail.com>
2025-11-10 20:25:27 +0800

    delete unnecessary files

--- generate-qr.js
+++
@@ -1,9 +0,0 @@
-const QRCode = require('qrcode');
-
-const data = JSON.stringify({"type":"tile","position":0});
-
-// Generate QR code in terminal
-QRCode.toString(data, { type: 'terminal', errorCorrectionLevel: 'H' }, function (err, url) {
-  if (err) throw err;
-  // QR code generated successfully
-});
